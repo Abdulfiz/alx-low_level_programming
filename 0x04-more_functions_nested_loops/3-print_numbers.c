225f990f871ddef23deb2264b07941a346f864d4
root <root@DESKTOP-B6AKN7M>
2022-09-16 03:04:50 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print number
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
